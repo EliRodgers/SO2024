a612fdcc837e42c62bddcd65bccc8e8579a27175
Rachel Jin <racheljin@Rachels-MacBook-Pro.local>
2024-03-25 16:18:32 -0700

    next added vite removed

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Head from "next/head";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "25th Annual Collegiate Wushu Tournament",
+  description: "Collegiates scoreboard",
+  icons: {
+    icon: "/wushulogo.svg",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+      Hello World
+    </div>
+  );
+}
