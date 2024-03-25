0b127e2ebcd0bb25e5f32a55b1bfdd3e869edd85
Rachel Jin <racheljin@Rachels-MacBook-Pro.local>
2024-03-24 23:27:54 -0700

    project created

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+  //this line defines a functional component named App. uses arrow function syntax. => function is a concise way of defining functions in JS.
+  return (
+    //react fragment
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        Hello world
+      </div>
+    </>
+  );
+};
+
+export default App;
