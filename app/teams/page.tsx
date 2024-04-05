4a3c77ef859343a59e12c8a75bab021f3e5a1f6c
Rachel Jin <racheljin24@gmail.com>
2024-04-04 18:49:08 -0700

    team search added

--- app/teams/page.tsx
+++ app/teams/page.tsx
@@ -24,17 +24,21 @@ export default async function Team({
   const query = searchParams?.query || "";
   const currentPage = Number(searchParams?.page) || 1;
   const teams = await getTeams();
+  // const filteredTeams = new Map<string, string[]>();
+  const filteredTeams = teams.filter((team: any) =>
+    JSON.stringify(team).toLowerCase().includes(query.toLowerCase())
+  );
+  // console.log(test);
   const mycolumns = ["name"];
 
   return (
     <div className="lg:text-lg animate-fade container lg:py-2 px-7 my-3 lg:my-10 overflow-hidden">
       <div className="font-grotesk my-5 lg:mt-0">
-        <SearchBar placeholder="Search competitors..." />
+        <SearchBar placeholder="Search teams..." />
       </div>
-      <Suspense key={query + currentPage} fallback={<NotFound />}>
-        {/* <Table query={query} currentPage={currentPage} /> */}
-      </Suspense>
-      {teams.map((team: any) => {
+      {/* <Suspense key={query + currentPage} fallback={<NotFound />}>
+      </Suspense> */}
+      {filteredTeams.map((team: any) => {
         return (
           <>
             <div className="lg:mb-2 lg:text-5xl font-grotesksc text-3xl bg-gradient-to-r from-light-gold via-orange-200 to-int-gold bg-clip-text text-transparent font-bold">
