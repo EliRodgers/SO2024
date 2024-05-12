b3ad2c34d5ada2666301fd6cbeb1f5c346740da3
lourdrickvalsote <lvalsote@stanford.edu>
2024-05-11 18:00:49 -0700

    Update header.tsx

--- app/header.tsx
+++ app/header.tsx
@@ -23,7 +23,7 @@ const Header = () => {
         />
       </div>
       {/* <div className="mt-[25rem] lg:text-8xl bg-gradient-to-r from-lightest-blue via-white to-lightest-blue bg-clip-text text-transparent text-[2.5rem] leading-10 font-bold font-grotesksc"> */}
-      <div className="relative lg:animate-fade mt-[7.2rem] md:mt-[25rem] lg:text-8xl text-[2.2rem] leading-[2.2rem] font-bold font-source-sans-pro">
+      <div className="relative lg:animate-fade mt-[1.2rem] md:mt-[15rem] lg:text-8xl text-[2.2rem] leading-[2.2rem] font-bold font-source-sans-pro">
         2024 Stanford Spring Open Tournament
         <div className="relative lg:text-6xl text-base bg-gradient-to-r from-red-600 via-red-600 to-red-600 bg-clip-text text-transparent font-bold italic font-source-sans-pro">
           May 25, 2024 • Stanford, CA
