34a5236a65118649c598a7dbb57e745b4a2b5468
lourdrickvalsote <lvalsote@stanford.edu>
2024-05-11 17:41:32 -0700

    Update header.tsx
    spacing pls work

--- app/header.tsx
+++ app/header.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const Header = () => {
   return (
-    <header className="overflow-hidden">
+    <header className="relative overflow-hidden">
       {/* <div className="flex lg:shrink-0 col-span-1 lg:py-5 items-center justify-center transition ease-in-out delay:150 duration:300 hover:scale-105 hover:animate-pulse">
         <div className="transform scale-75 lg:scale-100">
           <Button />
@@ -23,9 +23,9 @@ const Header = () => {
         />
       </div>
       {/* <div className="mt-[25rem] lg:text-8xl bg-gradient-to-r from-lightest-blue via-white to-lightest-blue bg-clip-text text-transparent text-[2.5rem] leading-10 font-bold font-grotesksc"> */}
-      <div className="lg:animate-fade mt-[7.2rem] md:mt-[25rem] lg:text-8xl text-[2.2rem] leading-[2.2rem] font-bold font-arial">
+      <div className="relative lg:animate-fade mt-[7.2rem] md:mt-[25rem] lg:text-8xl text-[2.2rem] leading-[2.2rem] font-bold font-arial">
         2024 Stanford Spring Open Tournament
-        <div className="lg:text-2xl text-base bg-gradient-to-r from-red-500 via-red-500 to-red-600 bg-clip-text text-transparent font-bold italic font-source-sans-pro">
+        <div className="relative lg:text-2xl text-base bg-gradient-to-r from-red-500 via-red-500 to-red-600 bg-clip-text text-transparent font-bold italic font-source-sans-pro">
           May 25, 2024 ⋆ Stanford, CA
         </div>
       </div>
